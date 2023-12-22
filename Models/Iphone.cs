d59223489e39a5c0a40f20282c8a6468f2658ed2
flinmt <vitorlimagerr.profissional@gmail.com>
2023-12-22 01:21:22 -0300

    feat: class Iphone, class Nokia

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,14 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Iphone(string Numero, string Modelo, string IMEI, int Memoria) : base(Numero, Modelo, IMEI, Memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
